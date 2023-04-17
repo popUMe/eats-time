6820412f8e9b437949ad98e688fcb996ef970b4e
Developer-Gilbert <Gilbert.dev.kr.gmail.com>
2023-04-17 19:14:30 +0900

    js작업중

--- eatsTime/WebContent/static/js/login.js
+++ eatsTime/WebContent/static/js/login.js
@@ -1,27 +1,26 @@
-const form = doucment.querySelector("form")
+const form = document.querySelector("form");
 
-form.addEventListener("submit", (e)=>{
-	e. preventDefault()
-	
-	const userid = form.userid.value
-	const password = form.password.value
-	
-	const authenticated = authentication(userid, password)
-	
-	if(authenticated){
-		alert("correct")
-	}else{
-		alert("wrong")
-	}
-}) 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const userid = form.userid.value;
+  const password = form.password.value;
+
+  const authenticated = authentication(userid, password);
+
+  if (authenticated) {
+    alert("Correct");
+  } else {
+    alert("Wrong");
+  }
+});
 
 // 아이디 패스워드 체크
 
-
-function authentication(userid, password){
-	if(userid === "gichul" && password === "1234"){
-		return true
-	}else{
-		return false
-	}
+function authentication(userid, password) {
+  if (userid === "gichul" && password === "1234") {
+    return true;
+  } else {
+    return false;
+  }
 }
