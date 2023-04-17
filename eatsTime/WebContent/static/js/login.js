6f99eb8b55f46ef0c82db89e7954ee3570b73286
Developer-Gilbert <Gilbert.dev.kr.gmail.com>
2023-04-17 19:05:59 +0900

    js작업중

--- eatsTime/WebContent/static/js/login.js
+++ eatsTime/WebContent/static/js/login.js
@@ -1,3 +1,12 @@
 const form = doucment.querySelector("form")
 
-form.addEventListener("submit", (e)=>	)
+form.addEventListener("submit", (e)=>{
+	e. preventDefault()
+	
+	const id = form.id.value
+	const password = form.password.value
+}) 
+
+// 아이디 패스워드 체크
+
+function authentication
