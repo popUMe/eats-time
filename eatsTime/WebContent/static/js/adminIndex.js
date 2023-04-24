999c22be0938ece69891f6f17e1fdb64bca7cf2e
event-jsy <chosey415@gmail.com>
2023-04-25 01:37:54 +0900

    병합

--- eatsTime/WebContent/static/js/adminIndex.js
+++ eatsTime/WebContent/static/js/adminIndex.js
@@ -1,7 +1,7 @@
 /**
  * 공지사항 
  */
-
+/*체크박스 */
 	$(document).ready(function() {
 			$("#chkAll").click(function() {
 				if($("#chkAll").is(":checked")) $("input[name=chk]").prop("checked", true);
@@ -17,6 +17,7 @@
 			});
 		});
 		
+/* 페이징 */
 		$(function() {
 			  var paging = $('.paging');
 			  var btns = paging.find('a');
@@ -27,6 +28,8 @@
 			  });
 			});
 		
+		
+	/* 버튼 클릭시 기능 */
 		$(function() {
 			  $('#add-btn').click(function() {
 			    location.href = 'adminNoticeWrite.jsp';
