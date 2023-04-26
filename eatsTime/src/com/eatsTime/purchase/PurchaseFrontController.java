7a0871348360292dc428e8ca407b4b6e65cd26ce
developer-cbum <dal.semicolon@gmail.com>
2023-04-26 18:42:24 +0900

    결제완료

--- eatsTime/src/com/eatsTime/purchase/PurchaseFrontController.java
+++ eatsTime/src/com/eatsTime/purchase/PurchaseFrontController.java
@@ -8,35 +8,39 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.eatsTime.Result;
-import com.eatsTime.member.controller.JoinOkController;
+import com.eatsTime.purchase.controller.PurchaseOkController;
+import com.eatsTime.purchase.controller.PurchasePayOkController;
 
 public class PurchaseFrontController extends HttpServlet {
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.setCharacterEncoding("UTF-8");
-		
-		String target = req.getRequestURI().replace(req.getContextPath() + "/", "").split("\\.")[0];
-		Result result = null;
+   @Override
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      req.setCharacterEncoding("UTF-8");
+      
+      String target = req.getRequestURI().replace(req.getContextPath() + "/", "").split("\\.")[0];
+      Result result = null;
 
-//		¿©±â if¹® else if¹® ¿¹½Ã´Ï±î °¢ ÇÁ·ÐÆ® ÄÁÆ®·Ñ·¯¿¡ ¸ÂÃç¼­ ¼öÁ¤ÇØ¼­ ÀÛ¾÷
-		if(target.equals("joinOk")) {
-			result = new JoinOkController().execute(req, resp);
-			
-		} else if(target.equals("joinOk")) {
-			result = new JoinOkController().execute(req, resp);
-		}
-		
-		if(result != null) {
-			if(result.isRedirect()) {
-				resp.sendRedirect(result.getPath());
-			}else {
-				req.getRequestDispatcher(result.getPath()).forward(req, resp);
-			}
-		}
-	}
-	
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		doGet(req, resp);
-	}
+//           if   else if      Ã´Ï±         Æ®   Æ® Ñ·       ç¼­      Ø¼   Û¾ 
+      if(target.equals("purchaseOk")) {
+         System.out.println("í”„ì»¨ë“¤ì–´ì˜´");
+         result = new PurchaseOkController().execute(req, resp);
+      } else if(target.equals("purchasePayOk")) {
+         result = new PurchasePayOkController().execute(req, resp);
+      }else if(target.equals("purchaseSuccess")) {
+         result = new Result();
+         result.setPath("templates/product/purchaseSuccess.jsp");
+      }
+      
+      if(result != null) {
+         if(result.isRedirect()) {
+            resp.sendRedirect(result.getPath());
+         }else {
+            req.getRequestDispatcher(result.getPath()).forward(req, resp);
+         }
+      }
+   }
+   
+   @Override
+   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      doGet(req, resp);
+   }
 }
--- eatsTime/src/com/eatsTime/purchase/domain/PurchaseVO.java
+++ eatsTime/src/com/eatsTime/purchase/domain/PurchaseVO.java
@@ -1,140 +1,142 @@
 package com.eatsTime.purchase.domain;
 
 public class PurchaseVO {
-	
-	private Long purId;
-	private Long memberId;
-	private Long productId;
-	private String purDate;
-	private String purName;
-	private String purPhoneNumber;
-	private String purAddress;
-	private String purAddressDetail;
-	private String purLocation;
-	private String purPw;
-	private boolean purDeliver;
+   
+   private Long purId;
+   private Long memberId;
+   private Long productId;
+   private String purDate;
+   private String purName;
+   private String purPhoneNumber;
+   private String purAddress;
+   private String purAddressDetail;
+   private String purLocation;
+   private String purPw;
+   private int purTotalPrice;
 
-	public PurchaseVO() {;}
+   public PurchaseVO() {;}
 
-	public Long getPurId() {
-		return purId;
-	}
+   public Long getPurId() {
+      return purId;
+   }
 
-	public void setPurId(Long purId) {
-		this.purId = purId;
-	}
+   public void setPurId(Long purId) {
+      this.purId = purId;
+   }
 
-	public Long getMemberId() {
-		return memberId;
-	}
+   public Long getMemberId() {
+      return memberId;
+   }
 
-	public void setMemberId(Long memberId) {
-		this.memberId = memberId;
-	}
+   public void setMemberId(Long memberId) {
+      this.memberId = memberId;
+   }
 
-	public Long getProductId() {
-		return productId;
-	}
+   public Long getProductId() {
+      return productId;
+   }
 
-	public void setProductId(Long productId) {
-		this.productId = productId;
-	}
+   public void setProductId(Long productId) {
+      this.productId = productId;
+   }
 
-	public String getPurDate() {
-		return purDate;
-	}
+   public String getPurDate() {
+      return purDate;
+   }
 
-	public void setPurDate(String purDate) {
-		this.purDate = purDate;
-	}
+   public void setPurDate(String purDate) {
+      this.purDate = purDate;
+   }
 
-	public String getPurName() {
-		return purName;
-	}
+   public String getPurName() {
+      return purName;
+   }
 
-	public void setPurName(String purName) {
-		this.purName = purName;
-	}
+   public void setPurName(String purName) {
+      this.purName = purName;
+   }
 
-	public String getPurPhoneNumber() {
-		return purPhoneNumber;
-	}
+   public String getPurPhoneNumber() {
+      return purPhoneNumber;
+   }
 
-	public void setPurPhoneNumber(String purPhoneNumber) {
-		this.purPhoneNumber = purPhoneNumber;
-	}
+   public void setPurPhoneNumber(String purPhoneNumber) {
+      this.purPhoneNumber = purPhoneNumber;
+   }
 
-	public String getPurAddress() {
-		return purAddress;
-	}
+   public String getPurAddress() {
+      return purAddress;
+   }
 
-	public void setPurAddress(String purAddress) {
-		this.purAddress = purAddress;
-	}
+   public void setPurAddress(String purAddress) {
+      this.purAddress = purAddress;
+   }
 
-	public String getPurAddressDetail() {
-		return purAddressDetail;
-	}
+   public String getPurAddressDetail() {
+      return purAddressDetail;
+   }
 
-	public void setPurAddressDetail(String purAddressDetail) {
-		this.purAddressDetail = purAddressDetail;
-	}
+   public void setPurAddressDetail(String purAddressDetail) {
+      this.purAddressDetail = purAddressDetail;
+   }
 
-	public String getPurLocation() {
-		return purLocation;
-	}
+   public String getPurLocation() {
+      return purLocation;
+   }
 
-	public void setPurLocation(String purLocation) {
-		this.purLocation = purLocation;
-	}
+   public void setPurLocation(String purLocation) {
+      this.purLocation = purLocation;
+   }
 
-	public String getPurPw() {
-		return purPw;
-	}
+   public String getPurPw() {
+      return purPw;
+   }
 
-	public void setPurPw(String purPw) {
-		this.purPw = purPw;
-	}
+   public void setPurPw(String purPw) {
+      this.purPw = purPw;
+   }
 
-	public boolean isPurDeliver() {
-		return purDeliver;
-	}
+   public int getPurTotalPrice() {
+      return purTotalPrice;
+   }
 
-	public void setPurDeliver(boolean purDeliver) {
-		this.purDeliver = purDeliver;
-	}
+   public void setPurTotalPrice(int purTotalPrice) {
+      this.purTotalPrice = purTotalPrice;
+   }
 
-	@Override
-	public String toString() {
-		return "PurchaseVO [purId=" + purId + ", memberId=" + memberId + ", productId=" + productId + ", purDate="
-				+ purDate + ", purName=" + purName + ", purPhoneNumber=" + purPhoneNumber + ", purAddress=" + purAddress
-				+ ", purAddressDetail=" + purAddressDetail + ", purLocation=" + purLocation + ", purPw=" + purPw
-				+ ", purDeliver=" + purDeliver + "]";
-	}
+   @Override
+   public String toString() {
+      return "PurchaseVO [purId=" + purId + ", memberId=" + memberId + ", productId=" + productId + ", purDate="
+            + purDate + ", purName=" + purName + ", purPhoneNumber=" + purPhoneNumber + ", purAddress=" + purAddress
+            + ", purAddressDetail=" + purAddressDetail + ", purLocation=" + purLocation + ", purPw=" + purPw
+            + ", purTotalPrice=" + purTotalPrice + "]";
+   }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((productId == null) ? 0 : productId.hashCode());
-		return result;
-	}
+   @Override
+   public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((purId == null) ? 0 : purId.hashCode());
+      return result;
+   }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		PurchaseVO other = (PurchaseVO) obj;
-		if (productId == null) {
-			if (other.productId != null)
-				return false;
-		} else if (!productId.equals(other.productId))
-			return false;
-		return true;
-	}
-	
+   @Override
+   public boolean equals(Object obj) {
+      if (this == obj)
+         return true;
+      if (obj == null)
+         return false;
+      if (getClass() != obj.getClass())
+         return false;
+      PurchaseVO other = (PurchaseVO) obj;
+      if (purId == null) {
+         if (other.purId != null)
+            return false;
+      } else if (!purId.equals(other.purId))
+         return false;
+      return true;
+   }
+
+
+   
 }
