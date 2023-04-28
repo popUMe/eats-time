7d53a89b27ec3ca4d33cce12a49ab76978699d8c
Developpeur-KCY <developpeur.kcy@gmail.com>
2023-04-28 19:02:06 +0900

    작업 보완중

--- eatsTime/WebContent/static/js/adminIndex.js
+++ eatsTime/WebContent/static/js/adminIndex.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-/**
- * 공지사항 
- */
-
-$(document).ready(function() {
-			$("#chkAll").click(function() {
-				if($("#chkAll").is(":checked")) $("input[name=chk]").prop("checked", true);
-				else $("input[name=chk]").prop("checked", false);
-			});
-			
-			$("input[name=chk]").click(function() {
-				var total = $("input[name=chk]").length;
-				var checked = $("input[name=chk]:checked").length;
-				
-				if(total != checked) $("#chkAll").prop("checked", false);
-				else $("#chkAll").prop("checked", true); 
-			});
-		});		
-=======
 $(document).ready(function() {
          $("#chkAll").click(function() {
             if($("#chkAll").is(":checked")) $("input[name=chk]").prop("checked", true);
@@ -30,4 +10,3 @@ $(document).ready(function() {
             else $("#chkAll").prop("checked", true);
          });
       });
->>>>>>> master
