7b1a44b7320c54b0b2e3362a774e1e8d1d726df8
Developpeur-KCY <developpeur.kcy@gmail.com>
2023-04-28 17:52:56 +0900

    수정

--- eatsTime/WebContent/static/js/adminIndex.js
+++ eatsTime/WebContent/static/js/adminIndex.js
@@ -1,33 +1,7 @@
-
-	
-
 /**
  * 공지사항 
  */
 
-<<<<<<< HEAD
-showList();
-
-
-function showList(){
-		const $table = $("table");
-		let text = "";
-		for(let i=0; i<notificationboards.length; i++){
-		text += `
-				<tr onclick="javascript:location.href='/viewOk.notificationboard?notbId=${notificationboards[i].notbId}'">
-			    <td><input type="checkbox" name="chk"></td>
-				<td name="notbId">공지번호: ${notificationboards[i].notbId}</td>
-				<td name="notbTitle"><a>${notificationboards[i].notbTitle}</a></td>
-				<td name="memberName">${notificationboards[i].memberName}</td>
-				<td name="notbDate">${notificationboards[i].notbDate}</td>
-			    </tr>
-				
-				`;
-		}
-		$table.append(text);
-	}
-=======
->>>>>>> b5a2917
 $(document).ready(function() {
 			$("#chkAll").click(function() {
 				if($("#chkAll").is(":checked")) $("input[name=chk]").prop("checked", true);
@@ -42,39 +16,5 @@ $(document).ready(function() {
 				else $("#chkAll").prop("checked", true); 
 			});
 		});
-		
-<<<<<<< HEAD
-		$(function() {
-			  var paging = $('.paging');
-			  var btns = paging.find('a');
-			  
-			  btns.click(function() {
-			    btns.removeClass('active');
-			    $(this).addClass('active');
-			  });
-			});
-		
-		
-		$(function() {
-			  $('#add-btn').click(function() {
-			    location.href = 'adminNoticeWrite.jsp';
-			  });
-			});
-
-		$(document).ready(function() {
-			  $("#delete-btn").click(function() {
-			    var checkedItems = $("input[name='chk']:checked");
-			    var checkedItemIds = [];
-			    checkedItems.each(function() {
-			      checkedItemIds.push($(this).closest("tr").find("td:nth-child(2)").text());
-			      $(this).closest("tr").remove();
-			    });
-			    console.log("Deleted item ids: " + checkedItemIds.join(", "));
-			  });
-			});
-
-=======
-
->>>>>>> b5a2917
 
 		
